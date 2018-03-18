11540141bcec616889ccbb6200c116947daa7af6
Jarkko Piiroinen <jmp@git.mail.kapsi.fi>
2018-03-18 11:37:50 +0200

    Minor code cleanup.

--- src/Hand.java
+++ src/Hand.java
@@ -1,7 +1,14 @@
 import java.util.Arrays;
 
 /** Utility methods for evaluating or creating a hand of cards. */
-public class Hand {
+public abstract class Hand {
+    /**
+     * Private constructor to disable instantiation of an abstract class.
+     */
+    private Hand() {
+
+    }
+
     /**
      * Poker hand evaluator based on Kevin Suffecool's 5-card hand evaluator and with Paul Senzee's pre-computed hash.
      * @param cards a hand of cards to evaluate
@@ -9,8 +16,9 @@ public class Hand {
      */
     public static int evaluate(Card[] cards) {
         // Only 5-card hands are supported
-        if (cards == null || cards.length != 5)
+        if (cards == null || cards.length != 5) {
             throw new IllegalArgumentException("Exactly 5 cards are required.");
+        }
 
         // Binary representations of each card
         final int c1 = cards[0].getValue();
@@ -20,20 +28,23 @@ public class Hand {
         final int c5 = cards[4].getValue();
 
         // No duplicate cards allowed
-        if (hasDuplicates(new int[]{c1, c2, c3, c4, c5}))
+        if (hasDuplicates(new int[]{c1, c2, c3, c4, c5})) {
             throw new IllegalArgumentException("Illegal hand.");
+        }
 
         // Calculate index in the flushes/unique table
         final int index = (c1 | c2 | c3 | c4 | c5) >> 16;
 
         // Flushes, including straight flushes
-        if ((c1 & c2 & c3 & c4 & c5 & 0xF000) != 0)
+        if ((c1 & c2 & c3 & c4 & c5 & 0xF000) != 0) {
             return Tables.Flushes.TABLE[index];
+        }
 
         // Straight and high card hands
         final int value = Tables.Unique.TABLE[index];
-        if (value != 0)
+        if (value != 0) {
             return value;
+        }
 
         // Remaining cards
         final int product = (c1 & 0xFF) * (c2 & 0xFF) * (c3 & 0xFF) * (c4 & 0xFF) * (c5 & 0xFF);
